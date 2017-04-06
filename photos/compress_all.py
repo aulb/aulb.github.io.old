8e26353732f3300cd50adf5326001ad644c988f9
aulbert <aalbertuntung@gmail.com>
2017-04-06 00:29:59 -0400

    Compression for now.
    Refine compression tomorrow.
    Finish first blog tomorrow.

--- photos/compress_all.py
+++ photos/compress_all.py
@@ -3,12 +3,11 @@ from PIL import Image
 from os import listdir
 from os.path import isfile, join
 
-path = './food'
+path = './food/'
 files = [f for f in listdir(path) if isfile(join(path, f)) and f[0] is not '.']
 
 
 for file in files:
-	print file
-	im = Image.open(join(path, f))
-	im.save(join(path, f), format='JPEG', quality=75)
-
+	print file	
+	with Image.open(join(path, file)) as im:
+		im.save(join(path, file), format='JPEG', quality=75)
