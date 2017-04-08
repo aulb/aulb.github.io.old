985b4ea4058171f3fbe9a0afc45243cfe4a764df
aulbert <aalbertuntung@gmail.com>
2017-04-08 00:39:37 -0400

    Photos compressed manually for now!
    - manual photo compression: solution for now

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 --
 Blog addition, writing underway.
 Merge pokeviz here!
-Merge language, erase japanese specific page
+Diverge language, unmerge languages
--- photos/compress_all.py
+++ photos/compress_all.py
@@ -1,13 +1,32 @@
 from PIL import Image
 
-from os import listdir
+from os import listdir, stat
 from os.path import isfile, join
 
 path = './food/'
 files = [f for f in listdir(path) if isfile(join(path, f)) and f[0] is not '.']
 
+size_limit = 100000 # 100kb
+max_iter = 10
 
-for file in files:
-	print file	
-	with Image.open(join(path, file)) as im:
-		im.save(join(path, file), format='JPEG', quality=75)
+def compress(filename, dest_filename=None, quality=50):
+	counter = 0
+	while stat(filename).st_size > size_limit and counter < max_iter:
+		with Image.open(filename) as im:
+			im.save(current_file, format='JPEG', quality=quality)
+		counter += 1
+
+
+for f in files:
+	current_file = join(path, f)
+	counter = 0
+	while stat(current_file).st_size > size_limit and counter < max_iter :
+		print current_file, counter, stat(current_file).st_size	
+		with Image.open(current_file) as im:
+			im.save(current_file, format='JPEG')
+
+		counter += 1
+
+
+		print stat(current_file).st_size
+
